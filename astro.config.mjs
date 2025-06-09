3794c40545a5a80bc8af61a249cb1f2d44a027ac
SoYisuss <yisusscc@gmail.com>
2025-06-09 01:44:54 -0600

    feature: implement base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // Sin integraciones de Tailwind, se manejará via PostCSS
+    base: '/Portfolio/',
 });
 
